dd630d259e8be5485012bfae276bc26e915da7a6
Đặng Nhật Khang <93917204+nhatkhang0910@users.noreply.github.com>
2025-12-14 14:26:43 +0700

    Adding Lesson8.html and Lesson8.js

---
+++ Lesson8/Lesson8.js
@@ -0,0 +1,37 @@
+// let a = "Xin chao cac ban hoc vien";
+// // cau 1
+// console.log(a[0]);
+// // cau 2
+// console.log(a.length);
+// //cau 3
+// for (let i = 1; i <= 50; i++) {
+//   console.log(i);
+// }
+// //cau 4
+// for (let i = 200; i >= 50; i--) {
+//   console.log(i);
+// }
+// //cau 5  <p id="text">Hello</p>
+
+// 3;
+
+// //cau 7
+// let foundZero = false;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === 0) {
+//     foundZero = true;
+//   }
+// }
+
+// if (foundZero) {
+//   console.log("có chứa số 0");
+// } else {
+//   console.log("không chứa số 0");
+// }
+
+// test = document.getElementById("mot").innerHTML;
+// console.log(test);
+
+hello = document.getElementById("text").innerHTML = "Xin chào";
+console.log(hello);
